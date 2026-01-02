acca61f2e460b95e08d172d4d2469f80e83128db
Chandana-Gajam <chandanagajam15@gmail.com>
2026-01-02 13:08:10 +0530

    Update Program 1.py

--- Program 1.py
+++ Program 1.py
@@ -1,3 +1,13 @@
+You are given a string "number" representing a positive integer and a character digit. Return the resulting string after removing exactly one occurrence of digit from number such that the value of the resulting string in decimal form is maximized. The test cases are generated such that digit occurs at least once in the number.
+
+I/P: number = "1321"
+
+digit = 1
+
+O/P: 321
+
+#Programs#
+
 n = str(input())
 d = str(input())
 ans = []
@@ -6,4 +16,5 @@ for i in range(len(n)):
     if n[i] == d:
         t = n[0:i] + n[(i+1):]
         ans.append(int(t))
+
 print(max(ans))        
