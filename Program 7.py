0375efa53f314ccd26c8f81db5479bce0b6e1ac2
Chandana-Gajam <chandanagajam15@gmail.com>
2026-01-02 19:38:03 +0530

    Update Program 7.py

--- Program 7.py
+++ Program 7.py
@@ -1,3 +1,21 @@
+Given a triangle array, return the minimum path sum from top to bottom.
+
+For each step, you may move to an adjacent number of the row below. More formally, if you are on index i on the current row, you may move to either index i or index 1 1 on the next row.
+
+Example 1:
+
+Input: triangle = [12],[3,4], [6,5,7), (4,1,8,31]
+
+Output: 11
+
+Explanation: The triangle Looks Like:
+                                  2
+                                3   4
+                             6    5    7
+                          4     1    8     3 
+
+# Program #
+
 from typing import List
 def minimmTotal(self,triangle: List[List[int]]) ->int:
     n=len(triangle)
@@ -12,4 +30,5 @@ def minimmTotal(self,triangle: List[List[int]]) ->int:
 
             triangle[i][j] +=min(left,right)
     
+
     return triangle[0][0]
