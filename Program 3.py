9311813bd8f39215c6ae3fd265f62fbe7454feef
Chandana-Gajam <chandanagajam15@gmail.com>
2026-01-02 13:41:48 +0530

    Update Program 3.py

--- Program 3.py
+++ Program 3.py
@@ -1,3 +1,19 @@
+You are given an array ARR which has N integers. You want to construct a new array RES using ARR by following the below algorithm:
+
+1. Initially, RES is empty
+2. Start at any index of ARR
+3. Choose a direction(left or right and iterate over the elements of ARR starting from the chosen index in the chosen direction
+4. Add each iteration element to the end of the RES Additionally, it is given that the array ARR is cyclic. This means that after the last element you will iterate to the first one and vice versa. The value of RES is the sum of the bitwise XOR value of all the prefixes of it. That means that the value of RES can be defined as follows:
+value(RES) = RES[0] + (RESIOLARES[1]) + (RES[0]^RES[1]^RES[2]) + ^RES[N-1])
+Find the maximum possible value of RES.
+
+Example:
+I/P: N= 10, ARR = [7855922016]
+O/P: 99
+considering RES = [5876102295] value(RES) = 5 + (5^8) + (5^8^7)+........ value(RES) = 5 + 13+10+12+13+13+15+13+4+1
+
+# Program #
+
 arr = [7, 8, 5, 5, 9, 2, 2, 0, 1, 6]
 n = len(arr)
 max_val = 0
@@ -18,4 +34,5 @@ for start in range(n):
         ans ^= i
         total += ans
     max_val = max(max_val,total)
+
 print(max_val)             
