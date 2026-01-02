e9839d76729026ee108355b8c31f2858f1b29b58
Chandana-Gajam <chandanagajam15@gmail.com>
2026-01-02 19:34:00 +0530

    Add files via upload

---
+++ Program 7.py
@@ -0,0 +1,15 @@
+from typing import List
+def minimmTotal(self,triangle: List[List[int]]) ->int:
+    n=len(triangle)
+
+    for i in range(n-2,-1,-1):
+        m=len(triangle[i])
+
+
+        for  j in range(m):
+            left=triangle[i+1][j]
+            right=triangle[i+1][j+1]
+
+            triangle[i][j] +=min(left,right)
+    
+    return triangle[0][0]
