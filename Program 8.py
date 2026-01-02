21ee6ecce4302f0b6c33e2c07c6488c75348b111
Chandana-Gajam <chandanagajam15@gmail.com>
2026-01-02 19:41:13 +0530

    Update Program 8.py

--- Program 8.py
+++ Program 8.py
@@ -1,3 +1,18 @@
+Given an array of T length each index contains array of two binary strings, your task is to return their maximum sum(Also a binary string).
+
+First Line contains T array Length, next n lines contain two string which shows binary space separated m,n. Find the sum of m and n in binary and print the maximum binary sum from the array elements.
+
+Input:
+3
+101 100
+110 10
+1000 11
+
+output:
+1011
+
+# Program #
+
 t = int(input())
 ans =[]
 
@@ -10,4 +25,5 @@ for i in range(t):
 
 print(ans)
 print(max(ans))
+
 print(bin(max(ans)))
